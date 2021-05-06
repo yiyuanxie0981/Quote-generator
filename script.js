0416bb53e2dc4ebdcdcb3be71506bce4d28ae5df
Carrie Xie <carriexie@Carries-MacBook-Air.local>
2021-05-06 13:41:52 +1000

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('testing');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+//Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading 
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+//Show New Quote
+function newQuote(){
+    loading();
+    //Pick a ramdom quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    }else{
+        authorText.textContent = author.text;
+    }
+    if (quote.text.length > 50) {
+        quoteText.classList.add("long-quote");
+    }else {
+        quoteText.classList.remove("long-quote");
+    }
+    //Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// Get Quote From API
+async function getQuote() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log('whoops, no quote', error);
+    }
+}
+
+// On Load
+// getQuote();
+// loading();
+complete()
